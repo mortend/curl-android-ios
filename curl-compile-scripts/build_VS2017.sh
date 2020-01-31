f59194847900fafacd267ae3dcd809bb2bdba684
Morten Daniel Fornes <mdfornes@gmail.com>
2020-01-31 11:59:39 +0700

    add build_VS2017.sh

---
+++ curl-compile-scripts/build_VS2017.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+real_path() {
+	[[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+REL_SCRIPT_PATH="$(dirname $0)"
+SCRIPTPATH=$(real_path $REL_SCRIPT_PATH)
+CURLPATH="$SCRIPTPATH/../curl"
+
+#Configure cURL
+cd $CURLPATH
+mkdir -p build-vc141 && cd build-vc141
+
+cmake -G"Visual Studio 15 2017 Win64" .. \
+    -DCMAKE_USE_WINSSL=ON \
+    -DBUILD_CURL_EXE=OFF \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DBUILD_TESTING=OFF
+
+cmake --build . -- //p:Configuration=Debug
+cmake --build . -- //p:Configuration=Release
+
+DESTDIR=$SCRIPTPATH/../prebuilt-with-ssl/vc141-x64-winssl
+cp -R lib/Debug lib/Release "$DESTDIR"
+
+#Copying cURL headers
+if [ -d "$DESTDIR/include" ]; then
+	echo "Cleaning headers"
+	rm -rf "$DESTDIR/include"
+fi
+cp -R $CURLPATH/include $DESTDIR/
+rm $DESTDIR/include/curl/.gitignore
+
+cd $PWD
+exit 0
